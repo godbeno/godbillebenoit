711d51da6047c208fa5fa818683781495c375ebe
godbille <clementgodbille@gmail.com>
2016-10-24 16:31:51 +0200

    oui

---
+++ src/engine/Deplacement.cpp
@@ -0,0 +1,10 @@
+#include "Deplacement.h"
+using namespace engine;
+
+Deplacement::Deplacement(int Originex, int Originey, int Ciblex, int Cibley)
+{
+   this->Originex = Originex;
+   this->Originey = Originey;
+   this->Ciblex = Ciblex;
+   this->Cibley = Cibley;
+}
---
+++ src/engine/Selection.cpp
@@ -0,0 +1,10 @@
+#include "Selection.h"
+using namespace engine;
+
+Selection::Selection(int Selectionx, int Selectiony)
+{
+   this->Originex = Originex;
+   this->Originey = Originey;
+   this->Ciblex = Ciblex;
+   this->Cibley = Cibley;
+}
