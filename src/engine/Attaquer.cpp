a0eb1ce46fdbc7304fa28f239c2c6435a3359055
godbille <clementgodbille@gmail.com>
2016-10-25 13:19:44 +0200

    attaquer.cpp

---
+++ src/engine/Attaquer.cpp
@@ -0,0 +1,12 @@
+#include "Attaquer.h"
+
+using namespace engine;
+
+
+Attaquer::Attaquer (int Attaquantx, int Attaquanty, int Ciblex, int Cibley)
+{
+   this->Attaquantx = Attaquantx;
+   this->Attaquanty = Attaquanty;
+   this->Ciblex = Ciblex;
+   this->Cibley = Cibley;
+}
